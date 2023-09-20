37e1e1241abe6591777db79f6e9ee56a54606868
Paul Mayer <paul@paul-mayer.eu>
2023-09-20 20:59:43 +0200

    add random system

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+fn print_position_system(query: Query<&Position>) {
+    for position in &query {
+        println!("position: {} {}", position.x, position.y);
+    }
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Update, print_position_system)
+        .run();
 }
